8c17fb1a7133d60e5f7dac3a4e823d79e53dd435
tr7zw <tr7zw@live.de>
2019-07-22 22:13:13 +0200

    Replaces Overwrite with terrible @ModifyVariable code

--- src/main/java/de/tr7zw/mod/mixins/PlayerRenderMixin.java
+++ src/main/java/de/tr7zw/mod/mixins/PlayerRenderMixin.java
@@ -1,13 +1,11 @@
 package de.tr7zw.mod.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import com.mojang.blaze3d.platform.GlStateManager;
 
 import de.tr7zw.mod.TrMod;
 import net.minecraft.client.MinecraftClient;
@@ -35,7 +33,7 @@ public abstract class PlayerRenderMixin extends LivingEntityRenderer<AbstractCli
 	@Inject(at = @At("RETURN"), method = "setModelPose")
 	private void setModelPose(AbstractClientPlayerEntity abstractClientPlayerEntity_1, CallbackInfo info) {
 		if(MinecraftClient.getInstance().options.perspective != 0 || !abstractClientPlayerEntity_1.isMainPlayer())return;
-		
+
 		PlayerEntityModel<AbstractClientPlayerEntity> playerEntityModel_1 = (PlayerEntityModel)this.getModel();
 		playerEntityModel_1.head.visible = false;
 		playerEntityModel_1.headwear.visible = false;
@@ -45,43 +43,96 @@ public abstract class PlayerRenderMixin extends LivingEntityRenderer<AbstractCli
 	@Shadow
 	abstract void setModelPose(AbstractClientPlayerEntity abstractClientPlayerEntity_1);
 
-	@Overwrite
-	public void method_4215(AbstractClientPlayerEntity abstractClientPlayerEntity_1, double x, double y, double z, float float_1, float float_2) {
-		if (!abstractClientPlayerEntity_1.isMainPlayer() || this.renderManager.camera != null && this.renderManager.camera.getFocusedEntity() == abstractClientPlayerEntity_1) {
-			double double_4 = y;
-			if (abstractClientPlayerEntity_1.isInSneakingPose()) {
-				double_4 = y;// - 0.125D;
-			}
+	AbstractClientPlayerEntity abstractClientPlayerEntity_1;
 
-			float bodyOffset;
-			if(abstractClientPlayerEntity_1 == MinecraftClient.getInstance().player && MinecraftClient.getInstance().options.perspective == 0) {
-				if(abstractClientPlayerEntity_1.isSneaking()){
-					bodyOffset = 0.27f;
-				}else if(MinecraftClient.getInstance().player.isInSwimmingPose()) {
-					bodyOffset = 0.60f;
-					if(abstractClientPlayerEntity_1.prevPitch > 0) {
-						double_4 += 0.6f * Math.sin(Math.toRadians(abstractClientPlayerEntity_1.prevPitch));
-					}else {
-						double_4 += 0.01f * -Math.sin(Math.toRadians(abstractClientPlayerEntity_1.prevPitch));
-						bodyOffset = 0.50f;
-					}
-				}else if(abstractClientPlayerEntity_1.hasVehicle()) {
-					bodyOffset = 0.2f;
-				}else{
-					bodyOffset = 0.25f;
-				}
-				//(player, player.posX - entity.posX + x + CameraTest.bodyOffset * Math.sin(Math.toRadians(renderOffset)), player.posY - entity.posY + y, player.posZ - entity.posZ + z - CameraTest.bodyOffset * Math.cos(Math.toRadians(renderOffset)), (float)renderOffset, ticks);
-				x += bodyOffset * Math.sin(Math.toRadians(abstractClientPlayerEntity_1.prevYaw));
-				z -= bodyOffset * Math.cos(Math.toRadians(abstractClientPlayerEntity_1.prevYaw));
-			}
-
-			this.setModelPose(abstractClientPlayerEntity_1);
-			GlStateManager.setProfile(GlStateManager.RenderMode.PLAYER_SKIN);
-			super.method_4054(abstractClientPlayerEntity_1, x, double_4, z, float_1, float_2);
-			GlStateManager.unsetProfile(GlStateManager.RenderMode.PLAYER_SKIN);
+	@ModifyVariable(method = "method_4215",
+			at = @At("HEAD"),
+			ordinal = 0
+			)
+	public AbstractClientPlayerEntity playerGetter(AbstractClientPlayerEntity abstractClientPlayerEntity_1) {
+		if(abstractClientPlayerEntity_1 == MinecraftClient.getInstance().player && MinecraftClient.getInstance().options.perspective == 0) {
+			this.abstractClientPlayerEntity_1 = abstractClientPlayerEntity_1;
+		}else {
+			this.abstractClientPlayerEntity_1 = null;
 		}
-
+		return abstractClientPlayerEntity_1;
 	}
 
+
+	@ModifyVariable(method = "method_4215",
+			at = @At("HEAD"),
+			ordinal = 0,
+			index = 1
+			)
+	public double playerCordX(double x) {
+		if (abstractClientPlayerEntity_1 != null && (!abstractClientPlayerEntity_1.isMainPlayer() || this.renderManager.camera != null && this.renderManager.camera.getFocusedEntity() == abstractClientPlayerEntity_1)) {
+
+			float bodyOffset;
+			if(abstractClientPlayerEntity_1.isSneaking()){
+				bodyOffset = 0.27f;
+			}else if(MinecraftClient.getInstance().player.isInSwimmingPose()) {
+				bodyOffset = 0.60f;
+				if(abstractClientPlayerEntity_1.prevPitch > 0) {
+				}else {
+					bodyOffset = 0.50f;
+				}
+			}else if(abstractClientPlayerEntity_1.hasVehicle()) {
+				bodyOffset = 0.2f;
+			}else{
+				bodyOffset = 0.25f;
+			}
+			x += bodyOffset * Math.sin(Math.toRadians(abstractClientPlayerEntity_1.prevYaw));
+		}
+
+		return x;
+	}	
+
+	@ModifyVariable(method = "method_4215",
+			at = @At("HEAD"),
+			ordinal = 1,
+			index = 2
+			)
+	public double playerCordY(double y) {
+		if (abstractClientPlayerEntity_1 != null && (!abstractClientPlayerEntity_1.isMainPlayer() || this.renderManager.camera != null && this.renderManager.camera.getFocusedEntity() == abstractClientPlayerEntity_1)) {
+
+			if(MinecraftClient.getInstance().player.isInSwimmingPose()) {
+				if(abstractClientPlayerEntity_1.prevPitch > 0) {
+					y += 0.6f * Math.sin(Math.toRadians(abstractClientPlayerEntity_1.prevPitch));
+				}else {
+					y += 0.01f * -Math.sin(Math.toRadians(abstractClientPlayerEntity_1.prevPitch));
+				}
+			}
+		}
+
+		return y;
+	}	
+
+	@ModifyVariable(method = "method_4215",
+			at = @At("HEAD"),
+			ordinal = 2,
+			index = 3
+			)
+	public double playerCordZ(double z) {
+		if (abstractClientPlayerEntity_1 != null && (!abstractClientPlayerEntity_1.isMainPlayer() || this.renderManager.camera != null && this.renderManager.camera.getFocusedEntity() == abstractClientPlayerEntity_1)) {
+
+			float bodyOffset;
+			if(abstractClientPlayerEntity_1.isSneaking()){
+				bodyOffset = 0.27f;
+			}else if(MinecraftClient.getInstance().player.isInSwimmingPose()) {
+				bodyOffset = 0.60f;
+				if(abstractClientPlayerEntity_1.prevPitch > 0) {
+				}else {
+					bodyOffset = 0.50f;
+				}
+			}else if(abstractClientPlayerEntity_1.hasVehicle()) {
+				bodyOffset = 0.2f;
+			}else{
+				bodyOffset = 0.25f;
+			}
+			z -= bodyOffset * Math.cos(Math.toRadians(abstractClientPlayerEntity_1.prevYaw));
+		}
+
+		return z;
+	}	
 
 }
