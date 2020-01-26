313ad2ab46c93d597ac3a1f696f41c2cd0d7bd5a
tr7zw <tr7zw@live.de>
2020-01-26 17:18:52 +0100

    Readd player offsets

--- src/main/java/de/tr7zw/firstperson/mixins/PlayerRenderMixin.java
+++ src/main/java/de/tr7zw/firstperson/mixins/PlayerRenderMixin.java
@@ -4,8 +4,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import de.tr7zw.firstperson.FirstPersonModelMod;
 import net.minecraft.client.MinecraftClient;
@@ -17,6 +17,7 @@ import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 
 @Mixin(PlayerEntityRenderer.class)
 public abstract class PlayerRenderMixin extends LivingEntityRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>>{
@@ -53,28 +54,16 @@ public abstract class PlayerRenderMixin extends LivingEntityRenderer<AbstractCli
 	private AbstractClientPlayerEntity abstractClientPlayerEntity_1;
 	private double realYaw;
 
-	/*@ModifyVariable(method = "render",
-			at = @At("HEAD"),
-			ordinal = 0
-			)
-	public AbstractClientPlayerEntity playerGetter(AbstractClientPlayerEntity abstractClientPlayerEntity_1) {
-		if(abstractClientPlayerEntity_1 == MinecraftClient.getInstance().player && MinecraftClient.getInstance().options.perspective == 0 && FirstPersonModelMod.isRenderingPlayer) {
-			this.abstractClientPlayerEntity_1 = abstractClientPlayerEntity_1;
+	@Inject(at = @At("HEAD"), method = "getPositionOffset", cancellable = true)
+	public void getPositionOffset(AbstractClientPlayerEntity var1, float var2, CallbackInfoReturnable<Vec3d> info) {
+		double x,y,z = x = y = z = 0;
+		if(var1 == MinecraftClient.getInstance().player && MinecraftClient.getInstance().options.perspective == 0 && FirstPersonModelMod.isRenderingPlayer) {
+			this.abstractClientPlayerEntity_1 = (AbstractClientPlayerEntity) var1;
 			realYaw = MathHelper.lerpAngleDegrees(MinecraftClient.getInstance().getTickDelta(), abstractClientPlayerEntity_1.prevYaw, abstractClientPlayerEntity_1.yaw);
 			 FirstPersonModelMod.isRenderingPlayer = false;
 		}else {
 			this.abstractClientPlayerEntity_1 = null;
 		}
-		return abstractClientPlayerEntity_1;
-	}
-
-
-	@ModifyVariable(method = "render",
-			at = @At("HEAD"),
-			ordinal = 0,
-			index = 1
-			)
-	public double playerCordX(double x) {
 		if (abstractClientPlayerEntity_1 != null && (!abstractClientPlayerEntity_1.isMainPlayer() || this.renderManager.camera != null && this.renderManager.camera.getFocusedEntity() == abstractClientPlayerEntity_1)) {
 			float bodyOffset;
 			if(abstractClientPlayerEntity_1.isSneaking()){
@@ -91,17 +80,7 @@ public abstract class PlayerRenderMixin extends LivingEntityRenderer<AbstractCli
 				bodyOffset = 0.25f;
 			}
 			x += bodyOffset * Math.sin(Math.toRadians(realYaw));
-		}
-		return x;
-	}	
-
-	@ModifyVariable(method = "render",
-			at = @At("HEAD"),
-			ordinal = 1,
-			index = 2
-			)
-	public double playerCordY(double y) {
-		if (abstractClientPlayerEntity_1 != null && (!abstractClientPlayerEntity_1.isMainPlayer() || this.renderManager.camera != null && this.renderManager.camera.getFocusedEntity() == abstractClientPlayerEntity_1)) {
+			z -= bodyOffset * Math.cos(Math.toRadians(realYaw));
 			if(MinecraftClient.getInstance().player.isInSwimmingPose()) {
 				if(abstractClientPlayerEntity_1.prevPitch > 0  && abstractClientPlayerEntity_1.isSubmergedInWater()) {
 					y += 0.6f * Math.sin(Math.toRadians(abstractClientPlayerEntity_1.prevPitch));
@@ -109,37 +88,11 @@ public abstract class PlayerRenderMixin extends LivingEntityRenderer<AbstractCli
 					y += 0.01f * -Math.sin(Math.toRadians(abstractClientPlayerEntity_1.prevPitch));
 				}
 			}
+			
 		}
-		return y;
-	}	
-
-	@ModifyVariable(method = "render",
-			at = @At("HEAD"),
-			ordinal = 2,
-			index = 3
-			)
-	public double playerCordZ(double z) {
-		if (abstractClientPlayerEntity_1 != null &&  (!abstractClientPlayerEntity_1.isMainPlayer() || this.renderManager.camera != null && this.renderManager.camera.getFocusedEntity() == abstractClientPlayerEntity_1)) {
-
-			float bodyOffset;
-			if(abstractClientPlayerEntity_1.isSneaking()){
-				bodyOffset = sneakBodyOffset;
-			}else if(MinecraftClient.getInstance().player.isInSwimmingPose()) {
-				if(abstractClientPlayerEntity_1.prevPitch > 0) {
-					bodyOffset = swimUpBodyOffset;
-				}else {
-					bodyOffset = swimDownBodyOffset;
-				}
-			}else if(abstractClientPlayerEntity_1.hasVehicle()) {
-				bodyOffset = inVehicleBodyOffset;
-			}else{
-				bodyOffset = 0.25f;
-			}
-			z -= bodyOffset * Math.cos(Math.toRadians(realYaw));
-		}
-		
-		return z;
-	}	*/
+		Vec3d vec = new Vec3d(x, y, z);
+		info.setReturnValue(vec);
+	}
 	
 	@Inject(at = @At("RETURN"), method = "render")
 	private void finishedRendering(AbstractClientPlayerEntity abstractClientPlayerEntity_1, float float_1, float float_2, MatrixStack matrixStack_1, VertexConsumerProvider vertexConsumerProvider_1, int int_1, CallbackInfo info) {
