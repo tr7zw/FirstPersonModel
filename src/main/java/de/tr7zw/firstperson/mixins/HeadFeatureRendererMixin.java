c3e6561cc1790ae74d78872204a3ce48ed58c1f1
KosmX <kosmx.mc@gmail.com>
2020-07-23 14:04:58 +0200

    fixed with optifine1.16.1 pre8 - render regions feature

--- src/main/java/de/tr7zw/firstperson/mixins/HeadFeatureRendererMixin.java
+++ src/main/java/de/tr7zw/firstperson/mixins/HeadFeatureRendererMixin.java
@@ -1,5 +1,6 @@
 package de.tr7zw.firstperson.mixins;
 
+import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,8 +25,7 @@ public abstract class HeadFeatureRendererMixin<T extends LivingEntity, M extends
 
 	@Inject(at = @At("HEAD"), method = "render", cancellable = true)
 	protected void render(MatrixStack matrixStack_1, VertexConsumerProvider vertexConsumerProvider_1, int int_1, T livingEntity_1, float float_1, float float_2, float float_3, float float_4, float float_5, float float_6, CallbackInfo info) {
-		if(FirstPersonModelMod.hideNextHeadItem) {
-			FirstPersonModelMod.hideNextHeadItem = false;
+		if(livingEntity_1 == MinecraftClient.getInstance().getCameraEntity() && FirstPersonModelMod.hideHeadWithMatrixStack == matrixStack_1) {
 	        info.cancel();
 		}
 	}
--- src/main/java/de/tr7zw/firstperson/mixins/PlayerRenderMixin.java
+++ src/main/java/de/tr7zw/firstperson/mixins/PlayerRenderMixin.java
@@ -1,9 +1,12 @@
 package de.tr7zw.firstperson.mixins;
 
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -33,9 +36,14 @@ public abstract class PlayerRenderMixin extends LivingEntityRenderer<AbstractCli
 	}
 
 
-	@Inject(at = @At("RETURN"), method = "setModelPose")
-	private void setModelPose(AbstractClientPlayerEntity abstractClientPlayerEntity_1, CallbackInfo info) {
-		if(FirstPersonModelMod.hideNextHeadItem) {
+	@Redirect(method = "render", at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/render/entity/PlayerEntityRenderer;setModelPose(Lnet/minecraft/client/network/AbstractClientPlayerEntity;)V"
+	))
+	private void setModelPoseRedirect(PlayerEntityRenderer playerEntityRenderer, AbstractClientPlayerEntity abstractClientPlayerEntity, AbstractClientPlayerEntity abstractClientPlayerEntity_1, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
+		setModelPose(abstractClientPlayerEntity);
+		if(abstractClientPlayerEntity == MinecraftClient.getInstance().getCameraEntity() && FirstPersonModelMod.hideHeadWithMatrixStack == matrixStack) {
+			this.setModelPose(abstractClientPlayerEntity);
 			PlayerEntityModel<AbstractClientPlayerEntity> playerEntityModel_1 = (PlayerEntityModel)this.getModel();
 			playerEntityModel_1.head.visible = false;
 			playerEntityModel_1.helmet.visible = false;
@@ -45,52 +53,9 @@ public abstract class PlayerRenderMixin extends LivingEntityRenderer<AbstractCli
 	@Shadow
 	abstract void setModelPose(AbstractClientPlayerEntity abstractClientPlayerEntity_1);
 
-	private AbstractClientPlayerEntity abstractClientPlayerEntity_1;
-	private double realYaw;
 
-	@Inject(at = @At("HEAD"), method = "getPositionOffset", cancellable = true)
-	public void getPositionOffset(AbstractClientPlayerEntity var1, float var2, CallbackInfoReturnable<Vec3d> info) {
-		if(MinecraftClient.getInstance().player.isSleeping() || FirstPersonModelMod.fixBodyShadow())return;
-		double x,y,z = x = y = z = 0;
-		if(var1 == MinecraftClient.getInstance().player && MinecraftClient.getInstance().options.perspective == 0 && FirstPersonModelMod.isRenderingPlayer) {
-			this.abstractClientPlayerEntity_1 = (AbstractClientPlayerEntity) var1;
-			realYaw = MathHelper.lerpAngleDegrees(MinecraftClient.getInstance().getTickDelta(), abstractClientPlayerEntity_1.prevYaw, abstractClientPlayerEntity_1.yaw);
-			 FirstPersonModelMod.isRenderingPlayer = false;
-		}else {
-			this.abstractClientPlayerEntity_1 = null;
-			return;
-		}
-		if (abstractClientPlayerEntity_1 != null && (!abstractClientPlayerEntity_1.isMainPlayer() || this.getRenderManager().camera != null && this.getRenderManager().camera.getFocusedEntity() == abstractClientPlayerEntity_1)) {
-			float bodyOffset;
-			if(abstractClientPlayerEntity_1.isSneaking()){
-				bodyOffset = FirstPersonModelMod.sneakBodyOffset + (FirstPersonModelMod.config.sneakXOffset / 100f);
-			}else if(MinecraftClient.getInstance().player.isInSwimmingPose()) {
-				abstractClientPlayerEntity_1.bodyYaw = abstractClientPlayerEntity_1.headYaw;
-				if(abstractClientPlayerEntity_1.prevPitch > 0) {
-					bodyOffset = FirstPersonModelMod.swimUpBodyOffset;
-				}else {
-					bodyOffset = FirstPersonModelMod.swimDownBodyOffset;
-				}
-			}else if(abstractClientPlayerEntity_1.hasVehicle()) {
-				bodyOffset = FirstPersonModelMod.inVehicleBodyOffset;
-			}else{
-				bodyOffset = 0.25f + (FirstPersonModelMod.config.xOffset / 100f);
-			}
-			x += bodyOffset * Math.sin(Math.toRadians(realYaw));
-			z -= bodyOffset * Math.cos(Math.toRadians(realYaw));
-			if(MinecraftClient.getInstance().player.isInSwimmingPose()) {
-				if(abstractClientPlayerEntity_1.prevPitch > 0  && abstractClientPlayerEntity_1.isSubmergedInWater()) {
-					y += 0.6f * Math.sin(Math.toRadians(abstractClientPlayerEntity_1.prevPitch));
-				}else {
-					y += 0.01f * -Math.sin(Math.toRadians(abstractClientPlayerEntity_1.prevPitch));
-				}
-			}
-			
-		}
-		Vec3d vec = new Vec3d(x, y, z);
-		abstractClientPlayerEntity_1 = null;
-		FirstPersonModelMod.isRenderingPlayer = false;
-		info.setReturnValue(vec);
-	}
+
+	//@Inject(at = @At("HEAD"), method = "getPositionOffset", cancellable = true)
+
 
 }
