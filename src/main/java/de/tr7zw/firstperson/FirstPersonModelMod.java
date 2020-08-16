43754d6a7b71b4022e67ac8417ffda97a979b429
KosmX <kosmx.mc@gmail.com>
2020-08-16 15:02:51 +0200

    updating to 1.16.2 :D (#34)
    * updating to 1.16.2 :D
    
    * Don't include modmenu, ignore latest log
    
    Co-authored-by: tr7zw <tr7zw@live.de>

--- src/main/java/de/tr7zw/firstperson/FirstPersonModelMod.java
+++ src/main/java/de/tr7zw/firstperson/FirstPersonModelMod.java
@@ -1,14 +1,14 @@
 package de.tr7zw.firstperson;
 
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.math.MatrixStack;
 import org.lwjgl.glfw.GLFW;
 
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 import me.sargunvohra.mcmods.autoconfig1u.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
-import net.fabricmc.fabric.api.client.keybinding.KeyBindingRegistry;
-import net.fabricmc.fabric.api.event.client.ClientTickCallback;
 import net.minecraft.util.Identifier;
 
 import javax.annotation.Nullable;
@@ -22,7 +22,7 @@ public class FirstPersonModelMod implements ModInitializer {
 	public static MatrixStack hideHeadWithMatrixStack = null;
 	public static boolean enabled = true;
 	public static FirstPersonConfig config = null;
-	private static FabricKeyBinding keyBinding;
+	private static KeyBinding keyBinding;
 	private static boolean isHeld = false;
 
 	public static boolean fixBodyShadow(MatrixStack matrixStack){
@@ -42,15 +42,15 @@ public class FirstPersonModelMod implements ModInitializer {
 		AutoConfig.register(FirstPersonConfig.class, GsonConfigSerializer::new);
 		config = AutoConfig.getConfigHolder(FirstPersonConfig.class).getConfig();
 		enabled = config.enabledByDefault;
-	    keyBinding = FabricKeyBinding.Builder.create(
-	            new Identifier("firstperson", "toggle"),
+	    keyBinding = new KeyBinding(
+	            new Identifier("firstperson", "toggle").getPath(),
 	            net.minecraft.client.util.InputUtil.Type.KEYSYM,
 	            GLFW.GLFW_KEY_F6,
 	            "Firstperson"
-	        ).build();
-	    KeyBindingRegistry.INSTANCE.addCategory("Firstperson");
-	    KeyBindingRegistry.INSTANCE.register(keyBinding);
-	    ClientTickCallback.EVENT.register(e ->
+	        );
+	    //KeyBindingRegistry.INSTANCE.addCategory("Firstperson");  It is added automatically now
+	    KeyBindingHelper.registerKeyBinding(keyBinding);
+	    ClientTickEvents.END_CLIENT_TICK.register(e ->
 	    {
 	        if(keyBinding.isPressed()) {
 	        	if(isHeld)return;
