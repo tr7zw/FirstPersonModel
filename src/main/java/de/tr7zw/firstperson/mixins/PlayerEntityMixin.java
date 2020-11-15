9f80b84c645bce1937109d42040b4b3a9dc9c36d
tr7zw <tr7zw@live.de>
2020-11-15 20:31:25 +0100

    Update PlayerEntityMixin.java

--- src/main/java/de/tr7zw/firstperson/mixins/PlayerEntityMixin.java
+++ src/main/java/de/tr7zw/firstperson/mixins/PlayerEntityMixin.java
@@ -4,6 +4,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import de.tr7zw.firstperson.PlayerSettings;
 import net.minecraft.entity.EntityType;
