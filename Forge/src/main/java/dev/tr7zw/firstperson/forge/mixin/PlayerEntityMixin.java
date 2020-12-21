6acec2a22bb8b139492945497111cec647f14913
tr7zw <tr7zw@live.de>
2020-12-21 18:11:27 +0100

    Shield blocking rotation and settings backend

--- Forge/src/main/java/dev/tr7zw/firstperson/forge/mixin/PlayerEntityMixin.java
+++ Forge/src/main/java/dev/tr7zw/firstperson/forge/mixin/PlayerEntityMixin.java
@@ -1,4 +1,4 @@
-package de.tr7zw.firstperson.fabric.mixins;
+package dev.tr7zw.firstperson.forge.mixin;
 
 import java.util.UUID;
 
@@ -11,72 +11,68 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import de.tr7zw.firstperson.FirstPersonModelCore;
 import de.tr7zw.firstperson.PlayerSettings;
 import de.tr7zw.firstperson.config.CosmeticSettings;
-import de.tr7zw.firstperson.fabric.FirstPersonModelMod;
-import de.tr7zw.firstperson.fabric.render.SolidPixelModelPart;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.entity.EntityDimensions;
-import net.minecraft.entity.EntityPose;
+import dev.tr7zw.firstperson.forge.FirstPersonModelMod;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.player.AbstractClientPlayerEntity;
+import net.minecraft.entity.EntitySize;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Pose;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-import net.minecraft.util.UseAction;
+import net.minecraft.item.UseAction;
 import net.minecraft.world.World;
 
-/**
- * Keep player specific settings, data and modifies the eye location when enabled
- *
- */
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity implements PlayerSettings {
 
-	protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
-		super(entityType, world);
+	protected PlayerEntityMixin(EntityType<? extends LivingEntity> type, World worldIn) {
+		super(type, worldIn);
+
 	}
 
 	private CosmeticSettings settings;
-	private SolidPixelModelPart headLayer;
-	private SolidPixelModelPart[] skinLayer;
-
+	
+	
 	@Inject(method = "<init>*", at = @At("RETURN"))
 	public void onCreate(CallbackInfo info) {
-		if (this.getUuidAsString().equals(MinecraftClient.getInstance().getSession().getUuid())) {
+		if (this.getCachedUniqueIdString().equals(Minecraft.getInstance().getSession().getPlayerID())) {
 			// the local var is not used for the own player
 		} else {
 			settings = new CosmeticSettings();
 			FirstPersonModelCore.syncManager.updateSettings(this);
 		}
 	}
+	
 
 	@Override
-	public SolidPixelModelPart[] getSkinLayers() {
-		return skinLayer;
+	public Object[] getSkinLayers() {
+		return null;
 	}
 	
 	@Override
 	public void setupSkinLayers(Object[] box) {
-		this.skinLayer = (SolidPixelModelPart[]) box;
+		
 	}
 	
 	@Override
-	public SolidPixelModelPart getHeadLayers() {
-		return headLayer;
+	public Object getHeadLayers() {
+		return null;
 	}
 	
 	@Override
 	public void setupHeadLayers(Object box) {
-		this.headLayer = (SolidPixelModelPart) box;
+		
 	}
 
 	@Override
 	public UUID getUUID() {
-		return super.uuid;
+		return super.entityUniqueID;
 	}
 
 	@Override
 	public void setCosmeticSettings(CosmeticSettings settings) {
-		if(((AbstractClientPlayerEntity)(Object)this).isMainPlayer()) {
+		if(((AbstractClientPlayerEntity)(Object)this).isUser()) {
 			settings.modifyCameraHeight = FirstPersonModelCore.config.cosmetic.modifyCameraHeight; // Not provided by the backend
 			FirstPersonModelCore.config.cosmetic = settings;
 			return;
@@ -86,15 +82,15 @@ public abstract class PlayerEntityMixin extends LivingEntity implements PlayerSe
 
 	@Override
 	public CosmeticSettings getCosmeticSettings() {
-		if(((AbstractClientPlayerEntity)(Object)this).isMainPlayer()) {
+		if(((AbstractClientPlayerEntity)(Object)this).isUser()) {
 			return FirstPersonModelCore.config.cosmetic;
 		}
 		return settings;
 	}
 
 	@SuppressWarnings("resource")
-	@Inject(method = "getActiveEyeHeight", at = @At("HEAD"), cancellable = true)
-	public float getActiveEyeHeight(EntityPose pose, EntityDimensions dimensions,
+	@Inject(method = "getStandingEyeHeight", at = @At("HEAD"), cancellable = true)
+	public float getStandingEyeHeight(Pose pose, EntitySize sizeIn,
 			CallbackInfoReturnable<Float> callback) {
 		float value = 0;
 		switch (pose) {
@@ -111,7 +107,7 @@ public abstract class PlayerEntityMixin extends LivingEntity implements PlayerSe
 		default:
 			value = 1.62f;
 		}
-		if (((PlayerEntity) (Object) this) == MinecraftClient.getInstance().player && FirstPersonModelMod.config.cosmetic.modifyCameraHeight) {
+		if (((PlayerEntity) (Object) this) == Minecraft.getInstance().player && FirstPersonModelMod.config.cosmetic.modifyCameraHeight) {
 			value *= ((float) FirstPersonModelMod.config.cosmetic.playerSize / 100f);
 		}
 		callback.setReturnValue(value);
@@ -121,22 +117,22 @@ public abstract class PlayerEntityMixin extends LivingEntity implements PlayerSe
 	@Inject(method = "tick", at = @At("RETURN"))
 	public void tick(CallbackInfo info) {
 		if(isBlockingFast()) {
-			this.bodyYaw = headYaw;
-			this.prevBodyYaw = prevHeadYaw;
+			this.renderYawOffset = rotationYawHead;
+			this.prevRenderYawOffset = prevRotationYawHead;
 		}
 	}
 	
 	public boolean isBlockingFast() {
-		if (this.isUsingItem() && !this.activeItemStack.isEmpty()) {
+		if (this.isHandActive() && !this.activeItemStack.isEmpty()) {
 			Item item = this.activeItemStack.getItem();
 			if (item.getUseAction(this.activeItemStack) != UseAction.BLOCK) {
 				return false;
 			} else {
-				return item.getMaxUseTime(this.activeItemStack) > 0;
+				return item.getUseDuration(this.activeItemStack) > 0;
 			}
 		} else {
 			return false;
 		}
 	}
-
+	
 }
