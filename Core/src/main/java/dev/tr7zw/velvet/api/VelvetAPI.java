6bf1c4a211cb57849354c1eb47535a3ab08f7a95
tr7zw <tr7zw@live.de>
2020-12-25 20:25:50 +0100

    On forge include cloth config but don't register it as a mod to prevent conflicts thanks to forges modloading

--- Core/src/main/java/dev/tr7zw/velvet/api/VelvetAPI.java
+++ Core/src/main/java/dev/tr7zw/velvet/api/VelvetAPI.java
@@ -3,6 +3,12 @@ package dev.tr7zw.velvet.api;
 import dev.tr7zw.velvet.api.config.ConfigBuilder;
 import dev.tr7zw.velvet.api.wrapper.Wrapper;
 
+/**
+ * Used to wrap NMS and other mods like ClothConfig for Modloader independed usage.
+ * 
+ * @author tr7zw
+ *
+ */
 public interface VelvetAPI {
 	
 	public Wrapper getWrapper();
