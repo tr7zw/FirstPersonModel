e0b7fd134a065bc15ad373acf5bf15e4a20e5f22
tr7zw <tr7zw@live.de>
2021-01-12 17:00:42 +0100

    Hide armor arms when using vanilla hands

---
+++ Fabric/src/main/java/dev/tr7zw/firstperson/fabric/mixins/ArmorFeatureRendererMixin.java
@@ -0,0 +1,49 @@
+package dev.tr7zw.firstperson.fabric.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import dev.tr7zw.firstperson.FirstPersonModelCore;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.feature.ArmorFeatureRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ArmorItem;
+
+@Mixin(ArmorFeatureRenderer.class)
+public abstract class ArmorFeatureRendererMixin<T extends LivingEntity, M extends BipedEntityModel<T>, A extends BipedEntityModel<T>> extends
+FeatureRenderer<T, M>{
+
+	public ArmorFeatureRendererMixin(FeatureRendererContext<T, M> context) {
+		super(context);
+	}
+
+	private boolean hideShoulders = false;
+	
+	@Inject(method = "renderArmor", at = @At("HEAD"))
+	private void renderArmor(MatrixStack matrices, VertexConsumerProvider vertexConsumers, T livingEntity,
+			EquipmentSlot equipmentSlot, int i, A bipedEntityModel, CallbackInfo info) {
+		if(equipmentSlot == EquipmentSlot.CHEST && FirstPersonModelCore.instance.isFixActive(livingEntity, matrices) && FirstPersonModelCore.config.firstPerson.vanillaHands) {
+			hideShoulders = true;
+		}else {
+			hideShoulders = false;
+		}
+	}
+	
+	@Inject(method = "renderArmorParts", at = @At("HEAD"))
+	private void renderArmorParts(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i,
+			ArmorItem armorItem, boolean bl, A bipedEntityModel, boolean bl2, float f, float g, float h,
+			String string, CallbackInfo info) {
+		if(hideShoulders) {
+			bipedEntityModel.leftArm.visible = false;
+			bipedEntityModel.rightArm.visible = false;
+		}
+	}
+	
+}
---
+++ Forge/src/main/java/dev/tr7zw/firstperson/forge/mixin/BipedArmorLayerMixin.java
@@ -0,0 +1,45 @@
+package dev.tr7zw.firstperson.forge.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+
+import dev.tr7zw.firstperson.FirstPersonModelCore;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.entity.IEntityRenderer;
+import net.minecraft.client.renderer.entity.layers.BipedArmorLayer;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+
+@Mixin(BipedArmorLayer.class)
+public abstract class BipedArmorLayerMixin<T extends LivingEntity, M extends BipedModel<T>, A extends BipedModel<T>> extends LayerRenderer<T, M> {
+
+	public BipedArmorLayerMixin(IEntityRenderer<T, M> entityRendererIn) {
+		super(entityRendererIn);
+	}
+
+	private boolean hideShoulders = false;
+	
+	@Inject(method = "func_241739_a_", at = @At("HEAD"))
+	 private void renderArmor(MatrixStack matrices, IRenderTypeBuffer p_241739_2_, T livingEntity, EquipmentSlotType equipmentSlot, int p_241739_5_, A p_241739_6_, CallbackInfo info) {
+		if(equipmentSlot == EquipmentSlotType.CHEST && FirstPersonModelCore.instance.isFixActive(livingEntity, matrices) && FirstPersonModelCore.config.firstPerson.vanillaHands) {
+			hideShoulders = true;
+		}else {
+			hideShoulders = false;
+		}
+	 }
+	
+	@Inject(method = "setModelSlotVisible", at = @At("RETURN"))
+	protected void setModelSlotVisible(A bipedEntityModel, EquipmentSlotType slotIn, CallbackInfo info) {
+		if(hideShoulders) {
+			bipedEntityModel.bipedLeftArm.showModel = false;
+			bipedEntityModel.bipedRightArm.showModel = false;
+		}
+	}
+	
+}
