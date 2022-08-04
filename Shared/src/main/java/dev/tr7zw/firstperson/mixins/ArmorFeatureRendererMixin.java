12a428261cd56a61b9813f71519fe12ee75d53db
tr7zw <tr7zw@live.de>
2022-08-05 00:00:26 +0200

    Redo armor hiding, to hopefully now be fully compatible with all kinds of mod armor

--- Shared/src/main/java/dev/tr7zw/firstperson/mixins/ArmorFeatureRendererMixin.java
+++ Shared/src/main/java/dev/tr7zw/firstperson/mixins/ArmorFeatureRendererMixin.java
@@ -1,58 +1,52 @@
 package dev.tr7zw.firstperson.mixins;
 
-import net.minecraft.client.player.LocalPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import com.mojang.blaze3d.vertex.PoseStack;
+
 import dev.tr7zw.firstperson.FirstPersonModelCore;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.ArmorItem;
 
-@Mixin(HumanoidArmorLayer.class)
-public abstract class ArmorFeatureRendererMixin<T extends LivingEntity, M extends HumanoidModel<T>, A extends HumanoidModel<T>> extends
-RenderLayer<T, M>{
+// lower prio to run before fabric api
+@Mixin(value = HumanoidArmorLayer.class, priority = 100)
+public abstract class ArmorFeatureRendererMixin<T extends LivingEntity, M extends HumanoidModel<T>, A extends HumanoidModel<T>>
+        extends RenderLayer<T, M> {
 
-	public ArmorFeatureRendererMixin(RenderLayerParent<T, M> context) {
-		super(context);
-	}
+    private static Minecraft fpm_mc = Minecraft.getInstance();
+    
+    public ArmorFeatureRendererMixin(RenderLayerParent<T, M> context) {
+        super(context);
+    }
 
-	private boolean hideBody = false;
-	private boolean hideShoulders = false;
-	private boolean hideHelmet = false;
-	
-	@Inject(method = "renderArmorPiece", at = @At("HEAD"))
-	private void renderArmor(PoseStack matrices, MultiBufferSource vertexConsumers, T livingEntity,
-			EquipmentSlot equipmentSlot, int i, A bipedEntityModel, CallbackInfo info) {
-		hideShoulders = equipmentSlot == EquipmentSlot.CHEST && FirstPersonModelCore.isRenderingPlayer && FirstPersonModelCore.config.vanillaHands;
-		hideHelmet = equipmentSlot == EquipmentSlot.HEAD && FirstPersonModelCore.isRenderingPlayer;
-		hideBody = equipmentSlot == EquipmentSlot.CHEST && FirstPersonModelCore.isRenderingPlayer && (livingEntity instanceof LocalPlayer player && player.isSwimming());
-	}
-	
-	@Inject(method = "renderModel", at = @At("HEAD"))
-	private void renderArmorParts(PoseStack matrixStack, MultiBufferSource vertexConsumerProvider, int i,
-			ArmorItem armorItem, boolean bl, A bipedEntityModel, boolean bl2, float f, float g, float h,
-			String string, CallbackInfo info) {
+    @Inject(method = "renderArmorPiece", at = @At("HEAD"), cancellable = true)
+    private void renderArmor(PoseStack matrices, MultiBufferSource vertexConsumers, T livingEntity,
+            EquipmentSlot equipmentSlot, int i, A bipedEntityModel, CallbackInfo info) {
+        if(livingEntity != fpm_mc.cameraEntity) {
+            return;
+        }
+        if(equipmentSlot == EquipmentSlot.HEAD && FirstPersonModelCore.isRenderingPlayer) {
+            FirstPersonModelCore.LOGGER.warn("Hiding");
+            info.cancel();
+        }
+        if(equipmentSlot == EquipmentSlot.CHEST && FirstPersonModelCore.isRenderingPlayer
+                && (livingEntity instanceof LocalPlayer player && player.isSwimming())){
+            info.cancel();
+        }
+        if(equipmentSlot == EquipmentSlot.CHEST && FirstPersonModelCore.isRenderingPlayer
+                && FirstPersonModelCore.config.vanillaHands) {
+            info.cancel();
+        }
+    }
 
-		if (hideBody) {
-			bipedEntityModel.body.visible = false;
-		}
-
-		if(hideShoulders) {
-			bipedEntityModel.leftArm.visible = false;
-			bipedEntityModel.rightArm.visible = false;
-		}
-		if(hideHelmet) {
-			bipedEntityModel.head.visible = false;
-			bipedEntityModel.hat.visible = false;
-		}
-	}
-	
 }
