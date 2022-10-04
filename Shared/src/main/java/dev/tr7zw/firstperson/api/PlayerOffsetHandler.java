db3a83815d5911744ef4e0f319fa9b8c3837f905
tr7zw <tr7zw@live.de>
2022-10-05 00:25:36 +0200

    Add api/modsupport. Move the PlayerAnimator logic to load conditionally
    Co-Authored-By: KosmX <15692868+KosmX@users.noreply.github.com>

---
+++ Shared/src/main/java/dev/tr7zw/firstperson/api/PlayerOffsetHandler.java
@@ -0,0 +1,22 @@
+package dev.tr7zw.firstperson.api;
+
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.world.phys.Vec3;
+
+public interface PlayerOffsetHandler {
+
+    /**
+     * Allows custom model offsets during rendering. The current value is the value
+     * pre-calculated by the mod(or handlers running before). If the handler doesn't
+     * apply to the current situation, the expected behavior is to just return
+     * "current".
+     * 
+     * @param entity
+     * @param delta
+     * @param original
+     * @param current
+     * @return
+     */
+    public Vec3 applyOffset(AbstractClientPlayer entity, float delta, Vec3 original, Vec3 current);
+
+}
