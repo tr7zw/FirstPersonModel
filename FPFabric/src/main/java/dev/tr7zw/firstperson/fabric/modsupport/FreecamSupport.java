96ca4de6bd623a33b1fda2b7400e666e28778b45
tr7zw <tr7zw@live.de>
2022-11-06 22:41:24 +0100

    Don't add the FreecamSupport class for the wrong freecam mod, bodge fix #309

--- FPFabric/src/main/java/dev/tr7zw/firstperson/fabric/modsupport/FreecamSupport.java
+++ FPFabric/src/main/java/dev/tr7zw/firstperson/fabric/modsupport/FreecamSupport.java
@@ -5,6 +5,11 @@ import net.xolt.freecam.Freecam;
 
 public class FreecamSupport implements ActivationHandler {
 
+    public FreecamSupport() {
+        // making sure the constructor throws an exception, in case something is wrong.
+        Freecam.isEnabled();
+    }
+    
     @Override
     public boolean preventFirstperson() {
         return Freecam.isEnabled();
