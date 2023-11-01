3f2ffdcaff26bacdea5ebeba2d1c22722a37dd25
tr7zw <tr7zw@live.de>
2023-11-01 21:26:19 +0100

    Block rendering CustomHeadLayer

---
+++ Shared/src/main/java/dev/tr7zw/firstperson/mixins/CustomHeadLayerMixin.java
@@ -0,0 +1,27 @@
+package dev.tr7zw.firstperson.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+
+import dev.tr7zw.firstperson.FirstPersonModelCore;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.layers.CustomHeadLayer;
+import net.minecraft.world.entity.LivingEntity;
+
+//lower prio to run before other mods
+@Mixin(value = CustomHeadLayer.class, priority = 100)
+public class CustomHeadLayerMixin {
+
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    public void render(PoseStack poseStack, MultiBufferSource multiBufferSource, int i, LivingEntity livingEntity,
+            float f, float g, float h, float j, float k, float l, CallbackInfo info) {
+        if(FirstPersonModelCore.isRenderingPlayer) {
+            info.cancel();
+        }
+    }
+
+}
