bad861b9eaf1ba9902107d29b19b02de21776bc8
tr7zw <tr7zw@live.de>
2024-06-14 20:00:02 +0200

    Fix neoforge²

--- src/main/java/dev/tr7zw/firstperson/FirstPersonBootstrap.java
+++ src/main/java/dev/tr7zw/firstperson/FirstPersonBootstrap.java
@@ -8,24 +8,26 @@
 //$$@Mod("firstperson")
 //$$public class FirstPersonBootstrap {
 //$$
-//$$	public FirstPersonBootstrap() {
-//$$		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> FirstPersonModelMod::new);
-//$$	}
-//$$	
+//$$    public FirstPersonBootstrap() {
+//$$            DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> FirstPersonModelMod::new);
+//$$    }
+//$$    
 //$$}
 //#elseif NEOFORGE
 //$$package dev.tr7zw.firstperson;
 //$$
 //$$import net.neoforged.api.distmarker.Dist;
-//$$import net.neoforged.fml.DistExecutor;
+//$$import net.neoforged.fml.loading.FMLEnvironment;
 //$$import net.neoforged.fml.common.Mod;
 //$$
 //$$@Mod("firstperson")
 //$$public class FirstPersonBootstrap {
 //$$
-//$$	public FirstPersonBootstrap() {
-//$$		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> FirstPersonModelMod::new);
-//$$	}
-//$$	
+//$$    public FirstPersonBootstrap() {
+//$$            if(FMLEnvironment.dist == Dist.CLIENT) {
+//$$                new FirstPersonModelMod();
+//$$            }
+//$$    }
+//$$    
 //$$}
 //#endif
